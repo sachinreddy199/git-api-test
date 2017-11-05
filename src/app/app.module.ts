be9fd6c9254b0365a96fbdd2eca5a02a493b7a1c
Sachin <sachin@aptmails.com>
2017-11-05 17:08:50 -0600

    tested and works fine

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { MatButtonModule, MatInputModule, MatFormFieldModule, MatListModule, MatCardModule, MatMenuModule  } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { GitHubComponent } from './github/github.component';
+import { GitHubService } from './service/github.service';
+export const routes = [
+
+    {
+        path: 'githubAPIs',
+        component: GitHubComponent
+    }
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      GitHubComponent
+  ],
+  imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      FormsModule,
+      HttpModule,
+      RouterModule.forRoot(routes, { enableTracing: false }),
+      MatButtonModule, MatInputModule, MatFormFieldModule, MatListModule, MatCardModule, MatMenuModule
+  ],
+  providers: [GitHubService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/github/github.component.ts
@@ -0,0 +1,70 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { GitHubService } from '../service/github.service';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GitHubComponent implements OnInit {
+    githubUserName: string;
+    followers = {};
+    followersTree = {};
+    globalGithubAPICallCount = 0;
+
+    maxLevels: number = 3;
+
+    errorMessage: string = '';
+
+    constructor(private githubService: GitHubService) {
+
+    }
+
+    getGithubAccountFollowers(gitUser, level, githubUserFollowersTree) {
+        this.errorMessage = '';
+        let self = this;
+        
+        self.githubService.getFollowers(gitUser).subscribe(followers => {
+            //TODO just to avoid max call timeout issues
+            if(self.globalGithubAPICallCount > 200) {
+                throw "exit";
+            }
+            if (level > self.maxLevels) {
+                return;
+            }
+            
+            followers.forEach(function (fol, index) {
+                let followersFollowers = {};
+                followersFollowers['gitUser'] = fol.login;
+                followersFollowers['followers'] = [];
+                
+                githubUserFollowersTree['followers'].push(followersFollowers);
+                self.globalGithubAPICallCount++;
+                self.getGithubAccountFollowers.apply(self, [fol.login, level + 1, followersFollowers]);
+            });
+            level++;
+            console.log('Number of followers for [' + gitUser + '] is [' + followers + ']');
+            console.log('Followers:', JSON.stringify(githubUserFollowersTree));
+            console.log('Deep count is [' + level + ']');
+        }, err => {
+            console.log('Error occured while calling API', err);
+            if(err._body) {
+                let m = JSON.parse(err._body);
+                self.errorMessage = m.message;
+            }
+            
+        });
+        this.followers = JSON.stringify(this.followersTree);
+        console.log('Git followers', this.followers);
+    }
+
+    reset() {
+        this.githubUserName = '';
+        this.followers = {};
+    }
+
+    ngOnInit() {
+        this.followersTree['followers'] = [];
+     }
+
+}
